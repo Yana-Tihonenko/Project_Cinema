eaa29ff351321beaed41a8f4dd41664a9e6204d7
Yana Tykhonenko <tihonenkoyana@gmail.com>
2023-04-19 18:35:50 +0200

    Add new constructor Add  new field year Remain initId to idFilmLast

--- src/model/Film.java
+++ src/model/Film.java
@@ -6,19 +6,31 @@ import java.util.List;
 public class Film {
   private final int idFilm;
   private final String name;
+
+  private final  int year;
+
   private final String description;
   private final Genre genre;
   private List<Integer> rating;
 
-  private int initId = 1;
+  private static int idFilmLast;
 
-  public Film(String name, String description, Genre genre) {
-    this.idFilm = initId;
+  public Film(int idFilm,String name, int year, String description, Genre genre, List<Integer> rating) {
+    this.year = year;
+    this.idFilm = idFilm;
     this.name = name;
     this.description = description;
     this.genre = genre;
     this.rating = new ArrayList<>();
-    initId++;
+    idFilmLast++;
+  }
+  public Film(int idFilm,String name, int year, String description, Genre genre) {
+    this.year = year;
+    this.idFilm = idFilm;
+    this.name = name;
+    this.description = description;
+    this.genre = genre;
+    idFilmLast++;
   }
 
   public int getIdFilm() {
@@ -40,6 +52,27 @@ public class Film {
   public List<Integer> getRating() {
     return rating;
   }
+
+  public static int getIdFilmLast() {
+    return idFilmLast;
+  }
+
+  public static void setIdFilmLast(int idFilmLast) {
+    Film.idFilmLast = idFilmLast;
+  }
+
+  @Override
+  public String toString() {
+    return "Film{" +
+        "idFilm=" + idFilm +
+        ", name='" + name + '\'' +
+        ", year=" + year +
+        ", description='" + description + '\'' +
+        ", genre=" + genre +
+        '}';
+  }
+
+
 }
 
 
