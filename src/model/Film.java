e84b22da3a98d98c8db6f75d25a1d3276e5e8bdc
Tykhonenko Yana <tihonenkoyana@gmail.com>
2023-05-21 13:41:37 +0200

    Refactoring class Film

--- src/model/Film.java
+++ src/model/Film.java
@@ -1,30 +1,30 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Film {
   private final int idFilm;
   private final String name;
 
-  private final  int year;
+  private final int year;
 
   private final String description;
   private final Genre genre;
-  private List<Integer> rating;
-
+  private  List<Integer> rating;
   private static int idFilmLast;
 
-  public Film(int idFilm,String name, int year, String description, Genre genre, List<Integer> rating) {
+  public Film(int idFilm, String name, int year, String description, Genre genre, List<Integer> rating) {
     this.year = year;
     this.idFilm = idFilm;
     this.name = name;
     this.description = description;
     this.genre = genre;
-    this.rating = new ArrayList<>();
+    this.rating = rating;
     idFilmLast++;
   }
-  public Film(int idFilm,String name, int year, String description, Genre genre) {
+
+  public Film(int idFilm, String name, int year, String description, Genre genre) {
     this.year = year;
     this.idFilm = idFilm;
     this.name = name;
@@ -57,22 +57,33 @@ public class Film {
     return idFilmLast;
   }
 
+  public int getYear() {
+    return year;
+  }
+
   public static void setIdFilmLast(int idFilmLast) {
     Film.idFilmLast = idFilmLast;
   }
 
-  @Override
-  public String toString() {
-    return "Film{" +
-        "idFilm=" + idFilm +
-        ", name='" + name + '\'' +
-        ", year=" + year +
-        ", description='" + description + '\'' +
-        ", genre=" + genre +
-        '}';
+
+  public void setRating(List<Integer> ratingList) {
+    this.rating =ratingList;
+
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Film)) return false;
+    Film film = (Film) o;
+    return idFilm == film.idFilm && year == film.year && name.equals(film.name)
+        && Objects.equals(description, film.description) && genre == film.genre;
+  }
 
+  @Override
+  public int hashCode() {
+    return Objects.hash(idFilm, name, year, description, genre);
+  }
 }
 
 
