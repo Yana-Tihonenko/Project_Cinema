deff94d4a9bc51fb72d4b8f9f805d7ede64b74cb
Tykhonenko Yana <tihonenkoyana@gmail.com>
2023-05-21 13:43:23 +0200

    Refactoring class Seats

--- src/model/Seats.java
+++ src/model/Seats.java
@@ -1,24 +1,20 @@
 package model;
 
-public enum Seats {
-  PLACE_1(1),
-  PLACE_2(2),
-  PLACE_3(3),
-  PLACE_4(4),
-  PLACE_5(5),
-  PLACE_6(6),
-  PLACE_7(7),
-  PLACE_8(8),
-  PLACE_9(9),
-  PLACE_10(10),
-  PLACE_11(11),
-  PLACE_12(12),
-  PLACE_13(13);
 
-  public final int place;
+import java.util.List;
+
+public class Seats {
+
+  List<String> seats;
 
 
-  Seats(int place) {
-    this.place=place;
+  public List<String> getSeats() {
+    return seats;
+  }
+
+  public void setSeats(List<String> seats) {
+    this.seats = seats;
   }
 }
+
+
