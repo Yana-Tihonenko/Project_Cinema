11b325f471f4bf0f0188f870a8a92d90392dbb65
Tykhonenko Yana <tihonenkoyana@gmail.com>
2023-05-21 13:41:55 +0200

    Refactoring class Genre

--- src/model/Genre.java
+++ src/model/Genre.java
@@ -1,8 +1,24 @@
 package model;
 
 public enum Genre {
-  ACTION,
-  COMEDY,
-  MELODRAMA,
-  THRILLER,
+  ACTION(1, "Приключения"),
+  COMEDY(2, "Комедия"),
+  MELODRAMA(3, "Мелодрама"),
+  THRILLER(4, "Триллер"),
+  ;
+  private final int numberGenre;
+  private final String nameGenre;
+
+  Genre(int numberGenre, String nameGenre) {
+    this.numberGenre = numberGenre;
+    this.nameGenre = nameGenre;
+  }
+
+  public int getNumberGenre() {
+    return numberGenre;
+  }
+
+  public String getNameGenre() {
+    return nameGenre;
+  }
 }
